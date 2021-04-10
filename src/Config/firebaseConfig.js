72e91e56f5245a5682bcc4f86a6e1590fcaec4ca
vikash.patel1 <p.vikash2810@gmail.com>
2021-04-10 23:01:35 +0530

    first commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyB4JwzqP-dBqtdW5CAnDQ3hRZdKGqO7lsQ",
+  authDomain: "mygitapp-3a1ad.firebaseapp.com",
+  projectId: "mygitapp-3a1ad",
+  storageBucket: "mygitapp-3a1ad.appspot.com",
+  messagingSenderId: "23416763003",
+  appId: "1:23416763003:web:d679f1d17861fd9736a8de",
+  measurementId: "G-8E3NN85Q0R",
+};
